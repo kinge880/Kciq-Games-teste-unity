bf79edbd17e81cf2bdb37743fd68f549c7ab70f7
kinge880 <brunomaya10@hotmail.com>
2020-06-10 21:53:54 -0500

    Inicio da maquina de estado do player
    Mapa pela metade, inicio maquina de estado, inicio maquina de animações

---
+++ Assets/Scripts/playerScripts/playerFSM.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerFSM : MonoBehaviour
+{
+    [SerializeField] float walkSpeed, jumpForce;
+    [SerializeField] bool isJump;
+    private float horizontal;
+    private Rigidbody2D playerBody;
+    private Animator animations;
+    private SpriteRenderer playerSprite;
+    public enum playerState
+    {
+        Idle,
+        Walk,
+        Jump,
+        Fall
+    }
+
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        animations = GetComponent<Animator>();
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (isJump)
+        {
+            playerBody.AddForce(new Vector2(0, jumpForce));
+            isJump = false;
+        }
+
+        playerBody.velocity = new Vector2(horizontal * walkSpeed, playerBody.velocity.y);
+    }
+
+    public void setJump(InputAction.CallbackContext context)
+    {
+        isJump = context.performed;
+    }
+
+    public void setMovement(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<float>();
+
+        switch (horizontal)
+        {
+            case 1:
+                animations.SetBool("onWalk", true);
+                playerSprite.flipX = false;
+                break;
+            case -1:
+                animations.SetBool("onWalk", true);
+                playerSprite.flipX = true;
+                break;
+            default:
+                animations.SetBool("onWalk", false);
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/scollScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scollScript : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float relativeSpeed;
+    [SerializeField] float oldPositionX;
+
+    void Start()
+    {
+        if (relativeSpeed < 1)
+        {
+            relativeSpeed = 1;
+        }
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        oldPositionX = target.position.x;
+    }
+
+    void Update()
+    {
+        transform.Translate((target.position.x - oldPositionX)/ relativeSpeed, 0, 0);
+        oldPositionX = target.position.x;
+    }
+
+    void paralaxEffect()
+    {
+
+    }
+}
