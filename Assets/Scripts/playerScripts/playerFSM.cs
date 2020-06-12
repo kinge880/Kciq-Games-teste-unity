740e58f93149887608619a0be97e479c129018b2
kinge880 <brunomaya10@hotmail.com>
2020-06-12 12:27:57 -0500

    dash
    bug conhecido: o personagem pode rampar e oscilar entre animação de queda e movimento em rampas pequenas

--- Assets/Scripts/playerScripts/playerFSM.cs
+++ Assets/Scripts/playerScripts/playerFSM.cs
@@ -7,13 +7,19 @@ public class playerFSM : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 1.5f; //velocidade de movimento do player
     [SerializeField] float jumpForce = 110f; //força do pulo
-    [SerializeField] enum PlayerState { STANDING, JUMPING, FALLING, DOUBLEJUMP } 
+    [SerializeField] float dashDuration = 0.625f;
+    [SerializeField] float dashSpeed = 2.0f;
+    [SerializeField] enum PlayerState { STANDING, JUMPING, DOUBLEJUMP, DASHING } 
     [SerializeField] PlayerState state;
     [SerializeField] Transform groundCheck; //objeto que toca o chão realizar a verificação
     [SerializeField] LayerMask wathIsGround; //identifica o que é o chão
+    
     private bool is_on_floor; //verifica se o player ta no chão
     private bool isJump; //verifica se o player ta pulando
+    private bool isDash;
+    private int dashDirection;
     private float horizontal; //capta o movimento horizontal
+    private float dashTimeLeft;
     private Rigidbody2D playerBody;
     private Animator animations;
     private SpriteRenderer playerSprite;
@@ -40,6 +46,9 @@ public class playerFSM : MonoBehaviour
             case PlayerState.DOUBLEJUMP:
                 Double_Jump();
                 break;
+            case PlayerState.DASHING:
+                Dash();
+                break;
         }
 
     }
@@ -63,6 +72,7 @@ public class playerFSM : MonoBehaviour
     {
         Update_velocity();
         Jump_transition();
+        Dash_transition();
 
         if (horizontal == 0){
             animations.SetBool("onWalk", false);
@@ -91,8 +101,7 @@ public class playerFSM : MonoBehaviour
 
         if (is_on_floor)
         {
-            animations.SetBool("onFall", false);
-            animations.SetBool("onJump", false);
+            animations.Rebind();
             state = PlayerState.STANDING;
         }
 
@@ -102,6 +111,9 @@ public class playerFSM : MonoBehaviour
 
     public void Jump_transition()
     {
+        //verifica se o player ta no chão
+        is_on_floor = Physics2D.Linecast(transform.position, groundCheck.position, wathIsGround);
+
         if (isJump)
         {
             animations.SetBool("onWalk", false);
@@ -110,7 +122,7 @@ public class playerFSM : MonoBehaviour
             state = PlayerState.JUMPING;
         }
 
-        if (playerBody.velocity.y < -0.1) //botei -0.1 pq o unity gera valores para y muito pequenos por conta de erro de ponto flutuante como "-2.443569E-08", mas isso impede uma verificação de < 0
+        if (!is_on_floor && playerBody.velocity.y < -0.5) //botei -0.1 pq o unity gera valores para y muito pequenos por conta de erro de ponto flutuante como "-2.443569E-08", mas isso impede uma verificação de < 0
         {
             animations.SetBool("onWalk", false);
             state = PlayerState.JUMPING;
@@ -120,8 +132,8 @@ public class playerFSM : MonoBehaviour
 
     public void Double_Jump()
     {
-        animations.SetBool("onDoubleJump", true);
         Update_velocity();
+        animations.SetBool("onDoubleJump", true);
         //verifica se o player ta no chão
         is_on_floor = Physics2D.Linecast(transform.position, groundCheck.position, wathIsGround);
 
@@ -130,16 +142,57 @@ public class playerFSM : MonoBehaviour
             animations.Rebind();
             state = PlayerState.STANDING;
         }
+
+        playerBody.velocity = new Vector2(horizontal * walkSpeed, playerBody.velocity.y);
     }
 
     public void Double_jump_transition()
     {
+        Update_velocity();
+        
         if (isJump)
         {
             animations.SetBool("onFall", false);
-            playerBody.AddForce(new Vector2(0, jumpForce));
+            playerBody.velocity = new Vector2(playerBody.velocity.x, 0);
+            playerBody.AddForce(new Vector2(0, jumpForce * 1.4f));
             isJump = false;
             state = PlayerState.DOUBLEJUMP;
+        }
+    }
+
+    public void Dash()
+    {
+        animations.SetBool("onDash", true);
+        dashTimeLeft -= Time.deltaTime;
+       
+        if (dashTimeLeft <= 0)
+        {
+            animations.SetBool("onDash", false);
+            state = PlayerState.STANDING;
+        }
+
+        playerBody.velocity = new Vector2(dashDirection * dashSpeed, playerBody.velocity.y);
+    }
+
+    public void Dash_transition()
+    {
+        if (isDash)
+        {
+            //seta o tempo de duração do dash em uma váriavel reciclavel, com dashDuration podendo ser definido no inspector
+            dashTimeLeft = dashDuration;
+            animations.SetBool("onWalk", false);
+            isDash = false;
+            state = PlayerState.DASHING;
+
+            //captura as direção do player para jogar o dash nessa direção e impedir que ele altere durante o dash
+            if (playerSprite.flipX)
+            {
+                dashDirection = -1;
+            }
+            else
+            {
+                dashDirection = 1;
+            }
         }
     }
 
@@ -153,6 +206,11 @@ public class playerFSM : MonoBehaviour
     public void Set_movement(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<float>();
+    }
+
+    public void Set_dash(InputAction.CallbackContext context)
+    {
+        isDash = context.performed;
     }
 
 }
